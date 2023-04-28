5815e12d7eec3554e3e0e0cea09664cb315b6e6f
Kimbo Graham Sanz <kimbo.sanz@kodego.ph>
2023-04-28 20:24:00 +0800

    Basic Login example

--- login.js
+++ login.js
@@ -9,3 +9,45 @@
 */
 
 // Code here.
+
+// calls the built in module inside node js
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/login') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+            console.log(typeof body);
+        });
+
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            const userName = 'kimbosanz123';
+            const passWord = 'root123';
+
+            if (data.username === userName && data.password === passWord) {
+                // Here we're just echoing back the received data in the response
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.write('You have logged in successful');
+                res.end();
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('Wrong password or username is being supplied');
+                res.end();
+            }
+
+        });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found');
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
